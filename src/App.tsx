5d4e9faced4a1d2cd50703946103021b3b1f9fc9
Farouq Abdurrahman <farouq03@gmail.com>
2023-12-22 23:33:43 +0100

    add tab for main content section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
-import { Navbar } from "./component/Navbar";
+import { Navbar } from "./component/Navbar/Navbar";
 import { Page } from "./layout/Page";
+import { TabComponent } from "./component/Tab/Tab";
+import { Home } from "./pages/Home";
 
 export default function App() {
   return (
     <MantineProvider>
       <Page>
         <Navbar />
+        <TabComponent defaultValue={"home"} tabList={
+          [
+            { label: "home", content: <Home /> },
+          ]
+        }/>
       </Page>
     </MantineProvider>
   );
---
+++ src/component/Tab/Tab.tsx
@@ -0,0 +1,39 @@
+import { Tabs } from "@mantine/core";
+import React from "react";
+
+interface Tab {
+  label: string
+  content: React.ReactNode
+}
+
+interface TabProps {
+  defaultValue: string
+  tabList: Tab[]
+}
+
+export const TabComponent = (
+  { defaultValue, tabList }: TabProps
+) => {
+  return (
+    <Tabs defaultValue={defaultValue} variant='pills'>
+      <Tabs.List>
+        {
+          tabList.map((tab) => (
+            <Tabs.Tab key={tab.label} mb={'xl'} fw={'bold'} value={tab.label} c={'black'} fz={'md'} p={0} style={{
+              backgroundColor: 'transparent',
+            }}>
+              {tab.label}
+            </Tabs.Tab>
+          ))
+        }
+      </Tabs.List>
+      {
+        tabList.map((tab) => (
+          <Tabs.Panel key={tab.label} value={tab.label}>
+            {tab.content}
+          </Tabs.Panel>
+        ))
+      }
+    </Tabs>
+  );
+}
